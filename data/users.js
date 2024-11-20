9ee5deea789f98dfb000fba51a2de8d33889b3f6
Shashank Neupane <suryaneupane0427@gmail.com>
2024-11-20 21:31:47 +0545

    update

--- data/users.js
+++ data/users.js
@@ -1,4 +1,4 @@
 export const users = [
-    { username: "sudeep", password: "sudeep123", page: "user-dash.html" },
+    { username: "shashank", password: "shashank123", page: "user-dash.html" },
     { username: "admin", password: "admin", page: "admin-dash.html" }
   ];
